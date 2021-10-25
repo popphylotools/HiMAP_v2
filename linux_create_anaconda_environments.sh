c8ad489c08fa64d4e9ed4dcdc24dc461c7fdf231
Oksana Vernygora <35181147+OksanaVe@users.noreply.github.com>
2021-10-25 15:31:21 -0400

    Update linux_create_anaconda_environments.sh

--- linux_create_anaconda_environments.sh
+++ linux_create_anaconda_environments.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash
 
-# create main python environment as well as environments for 3rd party tools
+# create main python environment with orthofinder
 conda env create -f environment.yml
-    # conda env create -f mafft_env.yml
-    # conda env create -f tapir_env.yml
-    # conda env create -f orthofinder_env.yml
-
-  # the tapir environment still needs hyphy2
-    # source activate tapir
-
-    # wget http://s3.faircloth-lab.org/packages/hyphy2.linux.gz
-    # gunzip hyphy2.*.gz
-    # chmod 0700 hyphy2.*
-    # mv hyphy2.* $(dirname $(which python))/hyphy2
 
 conda deactivate
